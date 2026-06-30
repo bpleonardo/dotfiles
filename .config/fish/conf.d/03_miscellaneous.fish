f5d5e2e35cfebcd0fdbb923a410c6059fc18172a
Leonardo <37600692+bpleonardo@users.noreply.github.com>
2026-06-29 21:10:39 -0300

    feat(nvim): set fish_vi_keybindings on launch

--- .config/fish/conf.d/03_miscellaneous.fish
+++ .config/fish/conf.d/03_miscellaneous.fish
@@ -1,3 +1,5 @@
 # I'm hardcoding the bin folder because `yarn global bin`
 # takes about 500ms.
+fish_vi_key_bindings
+
 fish_add_path -P "$HOME/.yarn/bin"
--- .config/fish/setup.fish
+++ .config/fish/setup.fish
@@ -2,6 +2,4 @@
 curl -sL https://git.io/fisher | source
 fisher update
 
-fish_vi_key_bindings
-
 fish_config theme save 'Catppuccin Mocha'
