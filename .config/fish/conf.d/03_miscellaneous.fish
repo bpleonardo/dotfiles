2d94e17a57c9b82164ce1ec742b6c64313c715aa
Leonardo <37600692+bpleonardo@users.noreply.github.com>
2026-07-15 10:30:13 -0300

    feat(nvim): upgrade fish_vi_keybindings

--- .config/fish/conf.d/03_miscellaneous.fish
+++ .config/fish/conf.d/03_miscellaneous.fish
@@ -1,5 +1,5 @@
 # I'm hardcoding the bin folder because `yarn global bin`
 # takes about 500ms.
-fish_vi_key_bindings
+set --global fish_key_bindings fish_vi_key_bindings
 
 fish_add_path -P "$HOME/.yarn/bin"
